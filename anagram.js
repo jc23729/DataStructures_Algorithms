0a795b33041aa448e52892ec788f37d2094598e7
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-07-24 08:06:31 -0400

    Update anagram.js

--- anagram.js
+++ anagram.js
@@ -1,2 +1,8 @@
 // Write a function to check whether two given strings are anagram of each other or not. An anagram of a string is another string that contains the same characters, only the order of characters can be different. For example, “abcd” and “dabc” are an anagram of each other.
 
+// function to check whether two given strings are anagram of each other or not.
+
+
+function areAnagram(str1, str2) {
+    
+}
